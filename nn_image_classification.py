c61c677a70825874c5122adba3cf0bb9dd5d0a32
fceja <88167774+fceja@users.noreply.github.com>
2024-07-14 15:49:18 -0700

    refactor into methods

---
+++ nn_image_classification.py
@@ -0,0 +1,96 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+
+# from collections import OrderedDict
+from nueral_net import NeuralNet
+
+
+def retrieve_and_load_data(batch_size):
+    # dataset preprocessing
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
+    )
+
+    # retrieve MNIST dataset
+    train_dataset = torchvision.datasets.MNIST(
+        root="./data", train=True, transform=transform, download=True
+    )
+    test_dataset = torchvision.datasets.MNIST(
+        root="./data", train=False, transform=transform
+    )
+
+    # load data
+    train_loader = torch.utils.data.DataLoader(
+        dataset=train_dataset, batch_size=batch_size, shuffle=True
+    )
+    test_loader = torch.utils.data.DataLoader(
+        dataset=test_dataset, batch_size=batch_size, shuffle=False
+    )
+
+    return train_loader, test_loader
+
+
+def test_model(device):
+    model.eval()
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        for images, labels in test_loader:
+            images = images.reshape(-1, 28 * 28).to(device)
+            labels = labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+        print(f"Test Accuracy: {100 * correct / total:.2f} %")
+
+
+def train_model(device, num_epochs):
+    # training loop
+    total_step = len(train_loader)
+    for epoch in range(num_epochs):
+        for i, (images, labels) in enumerate(train_loader):
+            images = images.reshape(-1, 28 * 28).to(device)
+            labels = labels.to(device)
+
+            # forward pass
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+
+            # backward and optimize
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if (i + 1) % 100 == 0:
+                print(
+                    f"Epoch [{epoch + 1}/{num_epochs}], Step [{i + 1}/{total_step}], Loss: {loss.item():.4f}"
+                )
+
+
+if __name__ == "__main__":
+    # device config
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # hyperparameters
+    num_epochs = 5
+    batch_size = 100
+    learning_rate = 0.001
+
+    # retrieve and load MNIST dataset
+    train_loader, test_loader = retrieve_and_load_data(batch_size)
+
+    # model init
+    model = NeuralNet(input_size=28 * 28, hidden_size=500, num_classes=10).to(device)
+
+    # loss and optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+    # train and test model
+    train_model(device, num_epochs)
+    test_model(device)
